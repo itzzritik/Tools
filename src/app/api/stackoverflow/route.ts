ed3148db46eda22e3fc239ec81e057210df85605
Ritik Srivastava <ritik.space@gmail.com>
2023-11-27 01:24:06 +0530

    ✨ Use new telegram api for telegram messages

--- src/app/api/stackoverflow/route.ts
+++ src/app/api/stackoverflow/route.ts
@@ -4,18 +4,19 @@ import { Browser } from 'puppeteer';
 import { CatchNextResponse, getBrowser } from '#utils/helper/helper';
 import { sendToTelegram } from '#utils/helper/sendMessage';
 
-let session = false;
-let error: TError;
+let logs: Array<string> = [];
 
 const login = async (browser: Browser) => {
 	const loginPage = await browser.newPage();
 	await loginPage.setViewport({ width: 1920, height: 1080 });
 	await loginPage.goto('https://stackoverflow.com/users/login');
-	await loginPage.waitForSelector('#password', { timeout: 2000 });
+	logs.push('		◦ Launched login page');
+	await loginPage.waitForSelector('#password', { timeout: 6000 });
 	await loginPage.type('#email', process.env.STACK_OVERFLOW_EMAIL ?? '');
 	await loginPage.type('#password', process.env.STACK_OVERFLOW_PASSWORD ?? '');
 
 	await (await loginPage.$('#password'))?.press('Enter');
+	logs.push('		◦ Logging in');
 	await loginPage.waitForNavigation();
 };
 
@@ -23,66 +24,61 @@ const checkSession = async (browser: Browser) => {
 	const settingsPage = await browser.newPage();
 	await settingsPage.setViewport({ width: 1920, height: 1080 });
 	await settingsPage.goto('https://stackoverflow.com/users/preferences');
-	await settingsPage.waitForSelector('#mainbar-full', { timeout: 2000 });
+	logs.push('		◦ Launched profile page');
+	await settingsPage.waitForSelector('#mainbar-full', { timeout: 6000 });
+	logs.push('		◦ Profile page worked, (Session Exists)');
 };
 
 const openRandomQuestion = async (browser: Browser) => {
 	const questionPage = await browser.newPage();
 	await questionPage.setViewport({ width: 1920, height: 1080 });
 	await questionPage.goto('https://stackoverflow.com/questions');
+	logs.push('		◦ Launched questions page');
 	const questionWrapper = await questionPage.$('#questions');
 	const questionList = (await questionWrapper?.$$('.s-post-summary > div.s-post-summary--content > h3 > a'))?.slice(0, 10);
 	const randomQuestion = questionList?.[Math.floor(Math.random() * questionList?.length)];
 
 	await randomQuestion?.click();
-	await questionPage.waitForNavigation({ timeout: 2000 });
+	logs.push('		◦ Random question clicked');
+	await questionPage.waitForNavigation({ timeout: 6000 });
+	logs.push('		◦ Question paged opened');
 };
 
 export async function GET () {
 	try {
-		session = false;
-		error = undefined as unknown as TError;
+		logs = [];
 
-		await sendToTelegram('[Started] Stack overflow session triggered');
+		logs.push('• Launching browser');
 		const browser = await getBrowser();
+		logs.push('		◦ Launched successfully');
 
 		try {
+			logs.push('• Logging to Stackoverflow');
 			await login(browser);
 		} catch (error) {
-			session = true;
-			console.log('Login: Already logged in');
+			logs.push('		◦ Error: Failed to login, session already exist');
 		}
 
 		try {
+			logs.push('• Checking Stackoverflow session');
 			await checkSession(browser);
 		} catch (err) {
-			if (!error) error = {} as TError;
-			error.session = err.message;
-			await sendToTelegram(`Error: ${err.message}`);
-			console.log(`CheckSession: ${err.message}`);
+			logs.push('		◦ Error: ' + err.message);
 		}
 
 		try {
+			logs.push('• Open random stackoverflow question');
 			await openRandomQuestion(browser);
 		} catch (err) {
-			if (!error) error = {} as TError;
-			error.question = err.message;
-			await sendToTelegram(`Error: ${err.message}`);
-			console.log(`OpenRandomQuestion: ${err.message}`);
+			logs.push('		◦ Error: ' + err.message);
 		}
 
-		const report = `[Completed${error ? ' (Error)' : ''}${session ? ' (Pre-LoggedIn)' : ''}] Stack overflow session`;
-
-		await sendToTelegram(report);
 		await browser.close();
+		logs.push('• Browser closed');
 
-		if (error) return NextResponse.json({ status: 500, ...error });
-
-		return NextResponse.json({ status: 200, message: report });
+		await sendToTelegram('Stackoverflow Session Triggered', logs);
+		return new NextResponse(['Stackoverflow Session Triggered\n', ...logs].map((v) => v.replaceAll('\t\t', '\t')).join('\n'));
 	} catch (err) {
-		console.log(err);
 		return CatchNextResponse(err);
 	}
 }
-
-type TError = {session: string, question: string}
--- src/utils/helper/sendMessage.ts
+++ src/utils/helper/sendMessage.ts
@@ -8,10 +8,26 @@ const options = {
 	second: 'numeric',
 } as const;
 
-export const sendToTelegram = async (message: string) => {
-	const currentDateIST = new Intl.DateTimeFormat('en-IN', options).format(new Date());
-	const url = `https://api.callmebot.com/text.php?user=ItzzRitik&text=${
-		encodeURIComponent(message) + ', on: ' + encodeURIComponent(currentDateIST)
-	}`;
-	await fetch(url);
+const generateMarkdown = (title: string, logs: Array<string>) => `
+*${title}*
+
+\`\`\`
+${logs.join('\n')}
+\`\`\`
+
+\`${new Intl.DateTimeFormat('en-IN', options).format(new Date())}\`
+`;
+
+export const sendToTelegram = async (title: string, logs: Array<string>) => {
+	const apiUrl = `https://api.telegram.org/bot${process.env.JARVIS_BOT_TOKEN}/sendMessage`;
+	const res = await fetch(apiUrl, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			chat_id: process.env.TELEGRAM_CHAT_ID,
+			text: generateMarkdown(title, logs),
+			parse_mode: 'MarkdownV2',
+		}),
+	});
+	await res.json();
 };
